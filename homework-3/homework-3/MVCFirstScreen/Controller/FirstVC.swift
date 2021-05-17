1e1f8d570606936d366711762f0a44c7c253bc6b
Vlad <vlad_sklyarov@mail.ru>
2021-05-17 08:09:04 +0200

    сделал первый экран с mvc

--- homework-3/homework-3/MVCFirstScreen/Controller/FirstVC.swift
+++ homework-3/homework-3/MVCFirstScreen/Controller/FirstVC.swift
@@ -10,24 +10,37 @@ import UIKit
 class FirstVC: UIViewController {
     
     @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var patronymLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     let model = DataModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setData(model)
         self.ageTextField.delegate = self
     }
     
     @IBAction func ageButtonAction() {
         var ageResponse = ""
-        if ageTextField.text == "27" {
+        if ageTextField.text == model.personInfo.age {
             ageResponse = "Ты угадал"
         }else{
             ageResponse = "Ты не угадал"
         }
         
-        let ageAlert = UIAlertController(title: "Мой возраст 27", message: ageResponse, preferredStyle: .alert)
+        let ageAlert = UIAlertController(title: "Мой возраст \(model.personInfo.age)", message: ageResponse, preferredStyle: .alert)
         ageAlert.addAction(UIAlertAction(title: "Понятно", style: .default))
         self.present(ageAlert, animated: true)
+    }
+    
+    func setData(_ model: DataModel) {
+        firstNameLabel.text = model.personInfo.firstName
+        secondNameLabel.text = model.personInfo.secondName
+        patronymLabel.text = model.personInfo.patronym
+        cityLabel.text = model.personInfo.city
     }
 }
 extension FirstVC: UITextFieldDelegate {
