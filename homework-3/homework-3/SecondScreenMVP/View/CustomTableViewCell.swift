23fdc2359393c7bc67daeb21b961cc10bc5d60a0
Vlad <vlad_sklyarov@mail.ru>
2021-05-22 07:21:32 +0200

    tmp

---
+++ homework-3/homework-3/SecondScreenMVP/View/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  homework-3
+//
+//  Created by user188734 on 5/22/21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
