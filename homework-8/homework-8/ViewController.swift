c2a4329cc35eec98cd9328d7551e79bae43b7c0b
Vlad <vlad_sklyarov@mail.ru>
2021-06-09 10:00:47 +0200

    init

---
+++ homework-8/homework-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-8
+//
+//  Created by user188734 on 6/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
